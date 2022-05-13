f0910e492035fae9535eb345fa68a15bc462fb64
luca_caltagirone <luca.caltagirone@protonmail.com>
2022-05-13 20:45:02 +0200

    Bug fixes and added flip x-axis during training

--- pcdet/datasets/jupyter/jupyter_dataset.py
+++ pcdet/datasets/jupyter/jupyter_dataset.py
@@ -20,21 +20,18 @@ class JupyterDataset(DatasetTemplate):
             logger=logger,
         )
 
-        self.lidar_files = glob.glob(str(self.root_path) + '/*/pcs/*.npy')
-        self.annotations = []
+        self.lidar_files = sorted(glob.glob(str(self.root_path) + '/*/*.npy'))
 
     def __len__(self):
-        return len(self.annotations)
+        return len(self.lidar_files)
 
     def __getitem__(self, index):
-        pointcloud = np.load(self.annotations[index][0], allow_pickle=True)
-        pointcloud = np.c_[pointcloud[:, 1], -pointcloud[:, 0], pointcloud[:, 2], pointcloud[:, 3] / 2 ** 16]
+        pointcloud = np.load(self.lidar_files[index], allow_pickle=True)
+        pointcloud = np.c_[pointcloud[:, 0], pointcloud[:, 1], pointcloud[:, 2] - 1.2, pointcloud[:, 3]/2**16]
         get_item_list = self.dataset_cfg.get("GET_ITEM_LIST", ["points"])
         input_dict = {"frame_id": index}
         if "points" in get_item_list:
             input_dict["points"] = pointcloud
-
-        input_dict.update({"gt_names": [], "gt_boxes": []})
         data_dict = self.prepare_data(data_dict=input_dict)
 
         return data_dict
