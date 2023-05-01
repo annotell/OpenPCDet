5b484628e8036b585667dcb172a730246c2cccbc
luca_caltagirone <luca.caltagirone@protonmail.com>
2023-05-01 10:37:10 +0200

    sparse cuboid grid in dataloader

--- pcdet/datasets/object_centric/object_centric_train_v1.py
+++ pcdet/datasets/object_centric/object_centric_train_v1.py
@@ -1,18 +1,15 @@
 import copy
-import glob
 import os.path
-
-import numpy as np
-import pandas as pd
-from scipy.spatial.transform import Rotation
-
 from ..dataset import DatasetTemplate
+import glob
+import pandas as pd
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 
 class Cube3D:
     def __init__(self, scale: np.ndarray, rotation: np.ndarray, coordinates: np.ndarray):
-        # Quaternions with order: (x, y, z, w)
-        self.rotation = np.asarray(rotation)
+        self.rotation = np.asarray(rotation)  # Quaternions with order: (x, y, z, w)
         self.coordinates = np.asarray(coordinates)  # X, Y, Z
         self.scale = np.asarray(scale)  # W, L, H
         self.rotation_matrix = Rotation.from_quat(self.rotation).as_matrix()
@@ -53,16 +50,12 @@ class Cube3D:
 
         # 3D bounding box corners. (Convention: x points forward, y to the left, z up.)
         corners = np.zeros((3, 8))
-        corners[0, :] = w / 2 * \
-            np.array([1, -1, -1, 1, 1, -1, -1, 1])  # x_corners
-        corners[1, :] = l / 2 * \
-            np.array([1, 1, 1, 1, -1, -1, -1, -1])  # y_corners
-        corners[2, :] = h / 2 * \
-            np.array([1, 1, -1, -1, 1, 1, -1, -1])  # z_corners
+        corners[0, :] = w / 2 * np.array([1, -1, -1, 1, 1, -1, -1, 1])  # x_corners
+        corners[1, :] = l / 2 * np.array([1, 1, 1, 1, -1, -1, -1, -1])  # y_corners
+        corners[2, :] = h / 2 * np.array([1, 1, -1, -1, 1, 1, -1, -1])  # z_corners
 
         # Rotate & Translate
-        corners = self.rotation_matrix @ corners + \
-            self.coordinates.reshape((3, -1))
+        corners = self.rotation_matrix @ corners + self.coordinates.reshape((3, -1))
 
         return corners
 
@@ -87,12 +80,10 @@ class Cube3D:
 
         # sample points randomly in box 1
         num_samples = 10000
-        points_in_box_1 = np.random.uniform(-0.5, 0.5, size=(
-            3, num_samples)) * self.get_scale().reshape(3, -1)
+        points_in_box_1 = np.random.uniform(-0.5, 0.5, size=(3, num_samples)) * self.get_scale().reshape(3, -1)
 
         # transformation from box_1 reference system to box_2 reference system
-        point_in_box_2 = R2_inv @ (R1 @ points_in_box_1 +
-                                   self.calculate_relative_position(shape_2=shape_2))
+        point_in_box_2 = R2_inv @ (R1 @ points_in_box_1 + self.calculate_relative_position(shape_2=shape_2))
 
         box_2_boundaries = shape_2.get_scale().reshape((3, -1)) / 2
 
@@ -101,8 +92,7 @@ class Cube3D:
         A2 = point_in_box_2 >= -box_2_boundaries
         A = np.logical_and(A1, A2)
         is_inside = np.prod(A, axis=0)
-        # fractions of box 1 in box 2
-        fraction_overlay = np.sum(is_inside) / is_inside.size
+        fraction_overlay = np.sum(is_inside) / is_inside.size  # fractions of box 1 in box 2
         intersection_volume = fraction_overlay * self.calculate_volume()
         return intersection_volume
 
@@ -113,8 +103,7 @@ class Cube3D:
         intersection_volume = self.calculate_intersection(shape_2=shape_2)
         if intersection_volume == 0:
             return 0
-        union_volume = self.calculate_volume() + shape_2.calculate_volume() - \
-            intersection_volume
+        union_volume = self.calculate_volume() + shape_2.calculate_volume() - intersection_volume
         return intersection_volume / union_volume
 
     def calculate_diagonal(self):
@@ -129,8 +118,7 @@ class Cube3D:
         :param points: N x 3 matrix of points in lidar coordinate system
         :return: N x 3 matrix of points in this box´s local coordinate system
         """
-        points_box = self.inverse_rotation_matrix @ (
-            points.T - self.coordinates.reshape((3, -1)))
+        points_box = self.inverse_rotation_matrix @ (points.T - self.coordinates.reshape((3, -1)))
         return points_box.T
 
     def points_inside(self, points_box):
@@ -165,11 +153,10 @@ class ObjectCentricTrainDataset(DatasetTemplate):
             logger=logger,
         )
         if training:
-            self.annotations = glob.glob(os.path.join(
-                str(self.root_path), '**/*.parquet'), recursive=True)
+            self.annotations = glob.glob(os.path.join(str(self.root_path), '**/*.parquet'), recursive=True)
         else:
-            self.annotations = glob.glob(os.path.join(
-                str(self.root_path), '**/*.parquet'), recursive=True)
+            self.annotations = glob.glob(os.path.join(str(self.root_path), '**/*.parquet'), recursive=True)
+
 
     @staticmethod
     def generate_prediction_dicts(
@@ -287,66 +274,29 @@ class ObjectCentricTrainDataset(DatasetTemplate):
         scale = cuboid.scale
         rotation = cuboid.rotation
         # sample uniform random point between coordinate - scale and coordinate + scale
-        delta_coordinates = np.random.uniform(
-            coordinates - scale / 2, coordinates + scale / 2)
+        delta_coordinates = np.random.uniform(coordinates - scale / 2, coordinates + scale / 2)
         # add noise to quaternion rotation
         delta_rotation = np.random.normal(rotation, 0.1)
-        noise_cuboid = Cube3D(coordinates=delta_coordinates,
-                              rotation=delta_rotation, scale=scale)
+        noise_cuboid = Cube3D(coordinates=delta_coordinates, rotation=delta_rotation, scale=scale)
 
         return noise_cuboid
 
-    @staticmethod
-    def generate_points_cuboid(height, width, length, spacing=15):
-        points = []
-        num_points_per_face = int(height / (spacing / 100))
-        xy_points = np.meshgrid(np.linspace(-width/2, width/2, num_points_per_face),
-                                np.linspace(-length/2, length/2,
-                                            num_points_per_face),
-                                indexing='ij')
-        xz_points = np.meshgrid(np.linspace(-width/2, width/2, num_points_per_face),
-                                np.linspace(-height/2, height/2,
-                                            num_points_per_face),
-                                indexing='ij')
-        yz_points = np.meshgrid(np.linspace(-length/2, length/2, num_points_per_face),
-                                np.linspace(-height/2, height/2,
-                                            num_points_per_face),
-                                indexing='ij')
-        points.append(np.vstack([xy_points[0].flatten(),
-                                xy_points[1].flatten(),
-                                np.full(num_points_per_face**2, -height/2)]).T)
-        points.append(np.vstack([xy_points[0].flatten(),
-                                xy_points[1].flatten(),
-                                np.full(num_points_per_face**2, height/2)]).T)
-        points.append(np.vstack([xz_points[0].flatten(),
-                                np.full(num_points_per_face**2, -length/2),
-                                xz_points[1].flatten()]).T)
-        points.append(np.vstack([xz_points[0].flatten(),
-                                np.full(num_points_per_face**2, length/2),
-                                xz_points[1].flatten()]).T)
-        points.append(np.vstack([np.full(num_points_per_face**2, -width/2),
-                                yz_points[0].flatten(),
-                                yz_points[1].flatten()]).T)
-        points.append(np.vstack([np.full(num_points_per_face**2, width/2),
-                                yz_points[0].flatten(),
-                                yz_points[1].flatten()]).T)
-        points = np.vstack(points)
-        points = np.hstack((points, 1*np.ones((points.shape[0], 1))))
-        return points
-
     def get_noise_pc_and_box(self, cuboid, points):
-        w, l, h = cuboid.scale
-        cuboid_points = self.generate_points_cuboid(h, w, l)
-        cuboid_points = np.hstack(
-            (cuboid_points, np.zeros((cuboid_points.shape[0], 1))))
+        # select a random subset of points costing of a percentage between 10% and 95% of the total number of points
+        percentage = np.random.uniform(0.1, 0.95)
+        cuboid_points = self.get_cuboid_points(cuboid, points)
+        n_points = int(percentage * cuboid_points.shape[0])
+        # select random points
+        random_idxs = np.random.choice(cuboid_points.shape[0], n_points, replace=False)
+        # select points
+        cuboid_points = cuboid_points[random_idxs, :]
+        # # add noise to points
+        cuboid_points[:, :3] = cuboid_points[:, :3] + np.random.normal(0, 0.05, cuboid_points[:, :3] .shape)
 
         noise_cuboid = self.random_box(cuboid)
-        points[:, :3] = noise_cuboid.transform_points_to_box_coordinate_system(
-            points[:, :3])
-        inv_quat = Rotation.from_matrix(
-            noise_cuboid.inverse_rotation_matrix).as_quat()
-        inv_coord = -(noise_cuboid.inverse_rotation_matrix @
-                      noise_cuboid.coordinates).T
+        points[:, :3] = noise_cuboid.transform_points_to_box_coordinate_system(points[:, :3])
+        inv_quat = Rotation.from_matrix(noise_cuboid.inverse_rotation_matrix).as_quat()
+        inv_coord = -(noise_cuboid.inverse_rotation_matrix @ noise_cuboid.coordinates).T
         cuboid_corrected = Cube3D(noise_cuboid.scale, inv_quat, inv_coord)
 
         # add column to noise_pc and fill with ones
@@ -359,43 +309,34 @@ class ObjectCentricTrainDataset(DatasetTemplate):
     def generate_sample(self, path):
         df = pd.read_parquet(path)
         points = np.load(path.replace('parquet', 'npy'))
-        cuboid = Cube3D(
-            df.Cube3D_scale[0], df.Cube3D_rotation[0], df.Cube3D_coordinates[0])
-        cuboid_local = Cube3D(df.Cube3D_scale[0], np.array(
-            [0, 0, 0, 1]), np.array([0, 0, 0]))
-        points[:, :3] = cuboid.transform_points_to_box_coordinate_system(
-            points[:, :3])
-        points, correct_cuboid = self.get_noise_pc_and_box(
-            cuboid_local, points)
+        cuboid = Cube3D(df.Cube3D_scale[0], df.Cube3D_rotation[0], df.Cube3D_coordinates[0])
+        cuboid_local = Cube3D(df.Cube3D_scale[0], np.array([0, 0, 0, 1]), np.array([0, 0, 0]))
+        points[:, :3] = cuboid.transform_points_to_box_coordinate_system(points[:, :3])
+        points, correct_cuboid = self.get_noise_pc_and_box(cuboid_local, points)
         annotation = self.get_annotation_from_cuboid(correct_cuboid)
         points = self.select_points(points)
-        points = np.c_[points[:, 1], -points[:, 0],
-                       points[:, 2], points[:, 3] / 2 ** 16, points[:, 4]]
+        points = np.c_[points[:, 1], -points[:, 0], points[:, 2], points[:, 3] / 2 ** 16, points[:, 4]]
         return points, annotation
 
     @staticmethod
     def get_annotation_from_cuboid(correct_cuboid):
-        annotation = {"name": ['Object'], "dimensions": [],
-                      "location": [], "rotation_y": []}
+        annotation = {"name": ['Object'], "dimensions": [], "location": [], "rotation_y": []}
         w, l, h = correct_cuboid.scale
         rot = correct_cuboid.rotation
         coord = correct_cuboid.coordinates
 
         new_coord = np.array([coord[1], -coord[0], coord[2]])
         new_scale = np.array([l, w, h])
-        new_rot = np.array(Rotation.from_quat(
-            rot).as_euler('xyz')[2] - np.pi / 2)
+        new_rot = np.array(Rotation.from_quat(rot).as_euler('xyz')[2] - np.pi / 2)
 
-        gt_boxes_lidar = np.concatenate(
-            [new_coord, new_scale, new_rot[..., np.newaxis]])
+        gt_boxes_lidar = np.concatenate([new_coord, new_scale, new_rot[..., np.newaxis]])
 
         annotation["dimensions"].append(new_scale)
         annotation["location"].append(new_coord)
         annotation["rotation_y"].append(new_rot)
         annotation["dimensions"] = np.array(annotation["dimensions"])
         annotation["location"] = np.array(annotation["location"])
-        annotation["rotation_y"] = np.array(
-            annotation["rotation_y"])[..., np.newaxis]
+        annotation["rotation_y"] = np.array(annotation["rotation_y"])[..., np.newaxis]
         annotation["gt_boxes_lidar"] = gt_boxes_lidar[np.newaxis, ...]
         annotation["name"] = np.array(annotation["name"])
 
@@ -407,15 +348,13 @@ class ObjectCentricTrainDataset(DatasetTemplate):
         pointcloud, annotation = self.generate_sample(self.annotations[index])
         while len(pointcloud) < 5:
             index = np.random.randint(0, len(self.annotations))
-            pointcloud, annotation = self.generate_sample(
-                self.annotations[index])
+            pointcloud, annotation = self.generate_sample(self.annotations[index])
 
         input_dict = {"frame_id": index}
         if "points" in get_item_list:
             input_dict["points"] = pointcloud
 
-        input_dict.update(
-            {"gt_names": annotation["name"], "gt_boxes": annotation["gt_boxes_lidar"]})
+        input_dict.update({"gt_names": annotation["name"], "gt_boxes": annotation["gt_boxes_lidar"]})
         data_dict = self.prepare_data(data_dict=input_dict)
 
         return data_dict
@@ -423,8 +362,7 @@ class ObjectCentricTrainDataset(DatasetTemplate):
     def evaluation(self, eval_det_annos, class_names):
         from .eval import get_official_eval_result
         from .object_centric_utils import transform_annotations_to_kitti_format
-        eval_gt_annos = [copy.deepcopy(
-            self.annotations[det['frame_id']][1]) for det in eval_det_annos]
+        eval_gt_annos = [copy.deepcopy(self.annotations[det['frame_id']][1]) for det in eval_det_annos]
         transform_annotations_to_kitti_format(eval_det_annos)
         transform_annotations_to_kitti_format(eval_gt_annos, info_with_fakelidar=self.dataset_cfg.get(
             'INFO_WITH_FAKELIDAR', False))
